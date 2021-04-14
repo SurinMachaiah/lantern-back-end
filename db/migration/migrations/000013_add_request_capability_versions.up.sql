017a7ff53c9011fe7ce98c1413a476ed7d7eccb5
Emily Michaud <59289146+emichaud998@users.noreply.github.com>
2021-04-14 13:41:43 -0400

    Change DB views to only grab endpoint infos that have requested version equal to empty string

--- db/migration/migrations/000013_add_request_capability_versions.up.sql
+++ db/migration/migrations/000013_add_request_capability_versions.up.sql
@@ -51,4 +51,36 @@ FOR EACH ROW
 WHEN (current_setting('metadata.setting', 't') IS NULL OR current_setting('metadata.setting', 't') = 'FALSE')
 EXECUTE PROCEDURE add_fhir_endpoint_info_history();
 
+CREATE or REPLACE VIEW org_mapping AS
+SELECT endpts.url, endpts.list_source, vendors.name as vendor_name, endpts.organization_names AS endpoint_names, orgs.name AS ORGANIZATION_NAME, orgs.secondary_name AS ORGANIZATION_SECONDARY_NAME, orgs.taxonomy, orgs.Location->>'state' AS STATE, orgs.Location->>'zipcode' AS ZIPCODE, links.confidence AS MATCH_SCORE
+FROM endpoint_organization AS links
+LEFT JOIN fhir_endpoints AS endpts ON links.url = endpts.url
+LEFT JOIN fhir_endpoints_info AS endpts_info ON endpts.url = endpts_info.url
+LEFT JOIN vendors ON endpts_info.vendor_id = vendors.id
+LEFT JOIN npi_organizations AS orgs ON links.organization_npi_id = orgs.npi_id
+WHERE endpts_info.requested_fhir_version = '';
+
+CREATE or REPLACE VIEW endpoint_export AS
+SELECT endpts.url, endpts.list_source, endpts.organization_names AS endpoint_names,
+    vendors.name as vendor_name,
+    endpts_info.tls_version, endpts_info.mime_types, endpts_metadata.http_response,
+    endpts_metadata.response_time_seconds, endpts_metadata.smart_http_response, endpts_metadata.errors,
+    endpts_info.capability_fhir_version AS FHIR_VERSION,
+    endpts_info.capability_statement->>'publisher' AS PUBLISHER,
+    endpts_info.capability_statement->'software'->'name' AS SOFTWARE_NAME,
+    endpts_info.capability_statement->'software'->'version' AS SOFTWARE_VERSION,
+    endpts_info.capability_statement->'software'->'releaseDate' AS SOFTWARE_RELEASEDATE,
+    endpts_info.updated_at AS INFO_UPDATED, endpts_info.created_at AS INFO_CREATED,
+    orgs.name AS ORGANIZATION_NAME, orgs.secondary_name AS ORGANIZATION_SECONDARY_NAME,
+    orgs.taxonomy, orgs.Location->>'state' AS STATE, orgs.Location->>'zipcode' AS ZIPCODE,
+    links.confidence AS MATCH_SCORE, endpts_info.supported_resources,
+    endpts_metadata.availability
+FROM endpoint_organization AS links
+RIGHT JOIN fhir_endpoints AS endpts ON links.url = endpts.url
+LEFT JOIN fhir_endpoints_info AS endpts_info ON endpts.url = endpts_info.url
+LEFT JOIN fhir_endpoints_metadata AS endpts_metadata ON endpts_info.metadata_id = endpts_metadata.id
+LEFT JOIN vendors ON endpts_info.vendor_id = vendors.id
+LEFT JOIN npi_organizations AS orgs ON links.organization_npi_id = orgs.npi_id
+WHERE endpts_info.requested_fhir_version = '';
+
 COMMIT;
--- db/sql/dbsetup.sql
+++ db/sql/dbsetup.sql
@@ -301,14 +301,15 @@ FROM endpoint_organization AS links
 LEFT JOIN fhir_endpoints AS endpts ON links.url = endpts.url
 LEFT JOIN fhir_endpoints_info AS endpts_info ON endpts.url = endpts_info.url
 LEFT JOIN vendors ON endpts_info.vendor_id = vendors.id
-LEFT JOIN npi_organizations AS orgs ON links.organization_npi_id = orgs.npi_id;
+LEFT JOIN npi_organizations AS orgs ON links.organization_npi_id = orgs.npi_id
+WHERE endpts_info.requested_fhir_version = '';
 
 CREATE or REPLACE VIEW endpoint_export AS
 SELECT endpts.url, endpts.list_source, endpts.organization_names AS endpoint_names,
     vendors.name as vendor_name,
     endpts_info.tls_version, endpts_info.mime_types, endpts_metadata.http_response,
     endpts_metadata.response_time_seconds, endpts_metadata.smart_http_response, endpts_metadata.errors,
-    endpts_info.capability_statement->>'fhirVersion' AS FHIR_VERSION,
+    endpts_info.capability_fhir_version AS FHIR_VERSION,
     endpts_info.capability_statement->>'publisher' AS PUBLISHER,
     endpts_info.capability_statement->'software'->'name' AS SOFTWARE_NAME,
     endpts_info.capability_statement->'software'->'version' AS SOFTWARE_VERSION,
@@ -323,7 +324,8 @@ RIGHT JOIN fhir_endpoints AS endpts ON links.url = endpts.url
 LEFT JOIN fhir_endpoints_info AS endpts_info ON endpts.url = endpts_info.url
 LEFT JOIN fhir_endpoints_metadata AS endpts_metadata ON endpts_info.metadata_id = endpts_metadata.id
 LEFT JOIN vendors ON endpts_info.vendor_id = vendors.id
-LEFT JOIN npi_organizations AS orgs ON links.organization_npi_id = orgs.npi_id;
+LEFT JOIN npi_organizations AS orgs ON links.organization_npi_id = orgs.npi_id
+WHERE endpts_info.requested_fhir_version = '';
 
 CREATE INDEX fhir_endpoints_url_idx ON fhir_endpoints (url);
 CREATE INDEX fhir_endpoints_info_url_idx ON fhir_endpoints_info (url);
